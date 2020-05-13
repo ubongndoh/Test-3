9211611c419b389cc80222e1f3cec3bafeeb0cb2
ubongndoh <ubongndoh@gmail.com>
2020-05-13 17:30:59 +0100

    section 1

---
+++ query.sql
@@ -0,0 +1,19 @@
+SELECT * FROM Users;
+
+SELECT * FROM Users WHERE Department = 'Accounting';
+
+SELECT * FROM Users WHERE Status = 'Pending';   
+
+SELECT * FROM Users WHERE ID BETWEEN 1 AND 5;  
+
+SELECT * FROM Users WHERE Age BETWEEN 19 AND 25;
+
+SELECT * FROM Users WHERE created_dt = '02-01-2020';
+
+SELECT * FROM Users WHERE Created_dt >= '2020-01-04' AND Created_dt <= '2020-01-06';
+
+UPDATE teamsUbong SET Status = 'Activated' WHERE Name IN ('Tim', 'Tolu', 'Ade');
+
+UPDATE Users SET Name = 'Timothy' WHERE ID = 2
+
+SELECT COUNT(id), department FROM Users GROUP BY department;
